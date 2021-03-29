6be93fd9bbedebf1867659336839495c3ce6fe64
NDPenaloza <ndpenaloza@gmail.com>
2021-03-28 23:48:02 -0700

    wrote portfolio page

---
+++ client/src/pages/Portfolio/index.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          This is the contact page
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Portfolio;
