7a87f942fd8b7aaac1c3fa0787b289a88666d8cf
NDPenaloza <ndpenaloza@gmail.com>
2021-03-28 23:39:41 -0700

    added routes and pages to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Index from '../src/pages/Index';
+import Home from '../src/pages/Home';
+import Portfolio from '../src/pages/Portfolio';
+import Contact from '../src/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Index}/>
+          <Route exact path='/home' component={Home}/>
+          <Route exact path='/portfolio' component={Portfolio}/>
+          <Route exact path='/contact' component={Contact}/>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
