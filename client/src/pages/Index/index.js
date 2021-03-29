5ccd394734f43733618c06bfe241fb094301f99d
NDPenaloza <ndpenaloza@gmail.com>
2021-03-28 23:47:11 -0700

    wrote index page

---
+++ client/src/pages/Index/index.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Index extends Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          This is the index page
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Index;
