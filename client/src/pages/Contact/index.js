99a85bda8c86c1b5b5dd0d24c284aba81a88a372
NDPenaloza <ndpenaloza@gmail.com>
2021-03-28 23:46:24 -0700

    wrote home and contact pages

---
+++ client/src/pages/Contact/index.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          This is the contact page
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Contact;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          This is the home page
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
