7b5f7fd6d1bba64e27f379e57543a064ec2e4246
NDPenaloza <ndpenaloza@gmail.com>
2021-03-31 01:02:57 -0700

    added buttons and animation

--- client/src/pages/Index/index.js
+++ client/src/pages/Index/index.js
@@ -1,11 +1,32 @@
 import { Component } from 'react';
+import { motion } from 'framer-motion';
 
 class Index extends Component {
   render() {
     return(
       <div>
-        <h1>Liza Penaloza</h1>
-        <h4>Photographer</h4>
+        <motion.h1
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 2}}>Liza Penaloza</motion.h1>
+        <motion.h4
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 3}}>Photographer</motion.h4>
+        <div className='row'>
+          <motion.a
+          initial={{x: -1000}}
+          animate={{x: 0}}
+          transition={{duration: 1}}className="waves-effect waves-light btn-large" href='/home'>Home</motion.a>
+          <motion.a
+          initial={{y: 1000}}
+          animate={{y: 0}}
+          transition={{duration: 1}}className="waves-effect waves-light btn-large" href='/portfolio'>Portfolio</motion.a>
+          <motion.a
+          initial={{x: 1000}}
+          animate={{x: 0}}
+          transition={{duration: 1}}className="waves-effect waves-light btn-large" href='/contact'>Contact</motion.a>
+        </div>
       </div>
     )
   }
